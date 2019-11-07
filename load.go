5d5c458e5ac78c42704f0d792276897c9440d292
stefba <st@rg-s.com>
2019-11-07 14:53:03 +0100

    Add load function

---
+++ load.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"text/template"
+)
+
+func load() error {
+	err := loadData()
+	if err != nil {
+		return err
+	}
+	return loadTemplate()
+}
+
+func loadData() error {
+	info, err := readInfo("./data/info.yaml")
+	if err != nil {
+		return err
+	}
+
+	works, err := readWorks("./data/work")
+	if err != nil {
+		return err
+	}
+
+	exp, err := readExperiences("./data/experience")
+	if err != nil {
+		return err
+	}
+
+	about, err := readAbout("./data/about/about.md")
+	if err != nil {
+		return err
+	}
+
+	siteDe = &site{
+		Home:       "/de/",
+		Sections:   sectionsLangs["de"],
+		Info:       info["de"],
+		Work:       works["de"],
+		Experience: exp["de"],
+		About:      about["de"],
+	}
+
+	siteEn = &site{
+		Home:       "/",
+		Sections:   sectionsLangs["en"],
+		Info:       info["en"],
+		Work:       works["en"],
+		Experience: exp["en"],
+		About:      about["en"],
+	}
+
+	return nil
+}
+
+func loadTemplate() error {
+	t, err := template.ParseFiles("./svea.html")
+	if err != nil {
+		return err
+	}
+	tmpl = t
+	return nil
+}
