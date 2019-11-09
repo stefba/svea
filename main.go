bf0d27fd852199d412dde9b9f45d7db18ff7a4e5
stefba <st@rg-s.com>
2019-11-09 14:11:49 +0100

    Add reload fucntion, make minor HTML optimizations

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//"strings"
+	"fmt"
 	"flag"
 	"log"
 	"net/http"
@@ -21,6 +22,7 @@ type site struct {
 }
 
 var root = "."
+var debug = false
 
 var siteEn = &site{}
 var siteDe = &site{}
@@ -28,10 +30,12 @@ var tmpl = &template.Template{}
 
 func main() {
 
+	permaReload := flag.Bool("reload", false, "reload files on every request")
 	path := flag.String("path", root+"", "set the root path of this app")
 	flag.Parse()
 
 	root = *path
+	debug = *permaReload
 
 	err := load()
 	if err != nil {
@@ -41,46 +45,66 @@ func main() {
 
 	http.HandleFunc("/", renderEn)
 	http.HandleFunc("/de/", renderDe)
+	http.HandleFunc("/rl/", reload)
 	http.HandleFunc("/static/", serveStatic)
+	http.HandleFunc("/googledbd0f1dfe416dbee.html", serveGoogle)
+
 	http.ListenAndServe(":8444", nil)
 
 }
 
-func renderEn(w http.ResponseWriter, r *http.Request) {
+func reload(w http.ResponseWriter, r *http.Request) {
 	err := load()
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		fmt.Fprint(w, err.Error())
 		return
+	}
+	fmt.Fprint(w, "did")
+}
+
+func renderEn(w http.ResponseWriter, r *http.Request) {
+	if debug {
+		err := load()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
 	}
 	if filepath.Ext(r.URL.Path) == ".jpg" {
 		http.ServeFile(w, r, root+"/data"+r.URL.Path)
 		return
 	}
-	err = tmpl.Execute(w, siteEn)
+	err := tmpl.Execute(w, siteEn)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 func renderDe(w http.ResponseWriter, r *http.Request) {
-	err := load()
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
+	if debug {
+		err := load()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
 	}
 	if filepath.Ext(r.URL.Path) == ".jpg" {
 		http.ServeFile(w, r, root+"/data"+r.URL.Path)
 		return
 	}
-	err = tmpl.Execute(w, siteDe)
+	err := tmpl.Execute(w, siteDe)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
+func serveGoogle(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, root+"/static/googledbd0f1dfe416dbee.html")
+}
+
 func serveStatic(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, root+""+r.URL.Path)
-	}
+	http.ServeFile(w, r, root+r.URL.Path)
+}
 /*
 func readSort() ([]string, error) {
 	b, err := ioutil.ReadFile(root + "/data/sort")
